6f95f7871aa235b0936d8ff1ff9d12c9d3737ea4
Tomas <T.sierra@redlink.com.ar>
2021-02-10 13:27:44 -0300

    dificultar y restart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
-
-
 @NgModule({
   declarations: [AppComponent, GrillaComponent],
   imports: [
--- src/app/grilla/grilla.component.ts
+++ src/app/grilla/grilla.component.ts
@@ -1,4 +1,7 @@
+import { Time } from '@angular/common';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { countriesList } from '../utils/utils';
 
 @Component({
   selector: 'app-grilla',
@@ -6,47 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grilla.component.css'],
 })
 export class GrillaComponent implements OnInit {
-  
-  score = 0;
-  moves = 0;
-  freeze = false;
+  difficulty = 2;
+  time: Time;
+  moves: number;
+  freeze: boolean;
   hideImage = '../../assets/images/hide.png';
-  selectedCards = [];
-  guessedId = []
-  cards = [
-    { text: 'One', showImage: '../../assets/images/brasil.svg', equalId: 1, id: 1 , show: false },
-    { text: 'Two', showImage: '../../assets/images/brasil.svg', equalId: 1, id: 2 , show: false },
-    { text: 'Three', showImage: '../../assets/images/argentina.svg', equalId: 2, id: 3, show: false },
-    { text: 'Four', showImage: '../../assets/images/argentina.svg', equalId: 2, id: 4 , show: false },
-    { text: 'Five', showImage: '../../assets/images/china.svg', equalId: 3, id: 5 , show: false },
-    { text: 'Six', showImage: '../../assets/images/china.svg', equalId: 3, id: 6 , show: false },
-    { text: 'Seven', showImage: '../../assets/images/usa.svg', equalId: 4, id: 7 , show: false },
-    { text: 'Eight', showImage: '../../assets/images/usa.svg', equalId: 4, id: 8 , show: false },
-    { text: 'Nine', showImage: '../../assets/images/germany.svg', equalId: 5, id: 9 , show: false },
-    { text: 'Ten', showImage: '../../assets/images/germany.svg', equalId: 5, id: 10 , show: false },
-    { text: 'Eleven', showImage: '../../assets/images/japan.svg', equalId: 6, id: 11 , show: false },
-    { text: 'Twelve', showImage: '../../assets/images/japan.svg', equalId: 6, id: 12 , show: false },
-    { text: 'Thirteen', showImage: '../../assets/images/macedonia.svg', equalId: 7, id: 13 , show: false },
-    { text: 'Fourteen', showImage: '../../assets/images/macedonia.svg', equalId: 7, id: 14 , show: false },
-    { text: 'Fifteen', showImage: '../../assets/images/mexico.svg', equalId: 8, id: 15 , show: false },
-    { text: 'Sixteen', showImage: '../../assets/images/mexico.svg', equalId: 8, id: 16 , show: false },
-    { text: 'Seventeen', showImage: '../../assets/images/portugal.svg', equalId: 9, id: 17 , show: false },
-    { text: 'Eighteen', showImage: '../../assets/images/portugal.svg', equalId: 9, id: 18 , show: false },
-    { text: 'Nineteen', showImage: '../../assets/images/spain.svg', equalId: 10, id: 19 , show: false },
-    { text: 'Twenty', showImage: '../../assets/images/spain.svg', equalId: 10, id: 20 , show: false },
-    { text: 'Twenty-one', showImage: '../../assets/images/venezuela.svg', equalId: 11, id: 21 , show: false },
-    { text: 'Twenty-two', showImage: '../../assets/images/venezuela.svg', equalId: 11, id: 22 , show: false },
-    { text: 'Twenty-three', showImage: '../../assets/images/wales.svg', equalId: 12, id: 23 , show: false },
-    { text: 'Twenty-four', showImage: '../../assets/images/wales.svg', equalId: 12, id: 24 , show: false },
-  ];
+  selectedCards: Array<any>;
+  guessedId: Array<any>;
+  cards: Array<any>;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.cards.sort(() => { return 0.5 - Math.random()});
+    this.moves = 0;
+    this.selectedCards = [];
+    this.guessedId = [];
+    this.freeze = false;
+    this.setCards();
+    this.cards.sort(() => {
+      return 0.5 - Math.random();
+    });
+    this.startTime();
   }
 
-  onClickCard(card: any) {
+  onClickCard(card: any): void {
     if (!this.freeze) {
       if (this.validateShowCard(card)) {
         this.selectedCards.push(card);
@@ -58,18 +44,17 @@ export class GrillaComponent implements OnInit {
     }
   }
 
-  private areEquals() {
+  private areEquals(): boolean {
     return this.selectedCards[0].equalId === this.selectedCards[1].equalId;
   }
 
-  private compareCards() {
+  private compareCards(): void {
     this.freeze = true;
     if (this.areEquals()) {
       this.guessedId.push(this.selectedCards[0].id);
       this.guessedId.push(this.selectedCards[1].id);
       this.selectedCards = [];
       this.freeze = false;
-      this.score += 120;
       // dont transform on hover
     } else {
       setTimeout(() => {
@@ -81,8 +66,33 @@ export class GrillaComponent implements OnInit {
     }
     this.moves++;
   }
-  private validateShowCard(card) {
-    return (this.selectedCards.length < 2 && this.selectedCards[0]?.id !== card.id && !this.guessedId.includes(card.id));
+  private validateShowCard(card): boolean {
+    return (
+      this.selectedCards.length < 2 &&
+      this.selectedCards[0]?.id !== card.id &&
+      !this.guessedId.includes(card.id)
+    );
   }
-  
+
+  setCards(): void {
+    if (this.difficulty === 1) {
+      // Easy
+      this.cards = JSON.parse(JSON.stringify(countriesList.slice(0, 12)));
+    } else if (this.difficulty === 2) {
+      // Medium
+      this.cards = JSON.parse(JSON.stringify(countriesList.slice(0, 16)));
+    } else {
+      // Hard
+      this.cards = JSON.parse(JSON.stringify(countriesList.slice(0, 24)));
+    }
+  }
+
+  changeDifficulty(number: number): void {
+    this.difficulty = number;
+    this.ngOnInit();
+  }
+
+  private startTime(): void {
+    // ToDo: timer
+  }
 }
